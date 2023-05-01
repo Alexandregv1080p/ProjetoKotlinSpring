fa4cba5598d1ead774971166d965346550797350
Alexandregv1080p <alexandregv1999@gmail.com>
2023-05-01 16:15:12 -0300

    Initial commit

---
+++ src/main/kotlin/me/firstkotlin/credit/request/system/CreditRequestSystemApplication.kt
@@ -0,0 +1,11 @@
+package me.firstkotlin.credit.request.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditRequestSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditRequestSystemApplication>(*args)
+}
---
+++ src/test/kotlin/me/firstkotlin/credit/request/system/CreditRequestSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.firstkotlin.credit.request.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditRequestSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
